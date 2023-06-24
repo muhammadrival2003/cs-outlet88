cc42e53efc2bdd5f53a0ba25a8a4777a7fce4ba9
Muhammad Rival <laporbuk141022@gmail.com>
2023-06-25 01:36:50 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.main')
+
+@section('container')
+<div class="row justify-content-center">
+    <div class="col-md-6 ps-5 pt-5 mb-5">
+        <h1 class="head-line">Temukan Fashion Yang Paling Cocok <br> Denganmu</h1>
+        <h6 class="description mt-4">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est, repellendus doloremque! Reprehenderit id sapiente provident perspiciatis velit beatae, corrupti dolores?</h6>
+        <div class="buttons-container mt-5">
+            <button class="button-arounder">SHOP NOW</button>
+        </div>
+    </div>
+    <div class="col-md-6 ps-5">
+        <img src="https://picsum.photos/id/111/300/300" alt="an old car">
+    </div>
+</div>
+@endsection
---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,41 @@
+<nav class="navbar navbar-expand-lg">
+    <div class="container">
+        <a class="navbar-brand" href="#">Cs_Outlet88</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse mx-auto" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">HOME</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">PROMO</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">SHOP</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">ABOUT</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav">
+                <li class="nav-item pe-3 d-flex">
+                    <div class="input-group">
+                        <input type="text" class="form-control rounded-5" placeholder="Search ..." aria-label="Recipient's username" aria-describedby="button-addon2">
+                    </div>
+                    <button class="btn btn-outline-secondary rounded-circle" type="button" id="button-addon2"><i class="bi bi-search"></i></button>
+                </li>
+                <li class="nav-item pe-4">
+                    <a href="#" class="btn" role="button" data-bs-toggle="button"><i class="bi bi-cart2"></i></a>
+                </li>
+                <li class="nav-item pe-4">
+                    <button type="button" class="btn btn-outline-secondary">LOGIN</button>
+                </li>
+                <li class="nav-item pe-4">
+                    <button type="button" class="btn btn-outline-secondary">SIGN UP</button>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
